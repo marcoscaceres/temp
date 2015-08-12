6fc5f2cf3088dc8d99ee326325ac7978d389e7dd
Marcos Caceres <marcos@marcosc.com>
2015-08-12 18:48:20 -0400

    Things

--- js/sw.js
+++ js/sw.js
@@ -1,53 +1,49 @@
+/*globals console, async, importScripts */
 "use strict";
-
+const CACHE_NAME = "newtab-v2";
 importScripts("async.js");
 
-const CACHE_NAME = "newtab-v2";
-const urlToCache = [
-  "../css/newTab.css",
-  "../css/newTab.inc.css",
-  "../js/async.js",
-  "../js/cells.js",
-  "../js/customize.js",
-  "../js/drag.js",
-  "../js/dragDataHelper.js",
-  "../js/drop.js",
-  "../js/dropPreview.js",
-  "../js/dropTargetShim.js",
-  "../js/grid.js",
-  "../js/newTab.js",
-  "../js/page.js",
-  "../js/rect.js",
-  "../js/sites.js",
-  "../js/transformations.js",
-  "../js/undo.js",
-  "../js/updater.js",
-  "../locale/newTab.json",
-  "../newTab.html"
-];
-
 self.addEventListener("install", (ev) => {
-  console.log("SW: Installing....")
+  const urlToCache = [
+    "../css/newTab.css",
+    "../css/newTab.inc.css",
+    "../js/async.js",
+    "../js/cells.js",
+    "../js/customize.js",
+    "../js/drag.js",
+    "../js/dragDataHelper.js",
+    "../js/drop.js",
+    "../js/dropPreview.js",
+    "../js/dropTargetShim.js",
+    "../js/grid.js",
+    "../js/newTab.js",
+    "../js/page.js",
+    "../js/rect.js",
+    "../js/sites.js",
+    "../js/transformations.js",
+    "../js/undo.js",
+    "../js/updater.js",
+    "../locale/newTab.json",
+    "../newTab.html"
+  ];
+  console.log("SW: Installing....");
   var populateCacheTask = async(function*() {
-    const cache = yield caches.open(CACHE_NAME);
+    var cache = yield caches.open(CACHE_NAME);
     cache.addAll(urlToCache);
-    console.log("SW: done populating the cache");
+    console.log("SW: done populating the cache.");
   });
   ev.waitUntil(populateCacheTask());
 });
 
+self.addEventListener('activate', function(event) {
+  console.log("SW activated");
+});
 
 self.addEventListener("fetch", (ev) => {
-  console.log("fetch event!", ev)
+  console.log("Fetch event!", ev)
   var lookInCacheTask = async(function*() {
-    const response = yield caches.match(ev.request)
-    // Cache hit - return response
-    if (response) {
-      console.log("SW: responding from cache");
-      return response;
-    }
-    console.log("SW: responding from network");
-    return fetch(ev.request);
+    var response = yield caches.match(ev.request);
+    return response || fetch(ev.request);
   });
   ev.respondWith(lookInCacheTask());
 });
